dd14787138ecb71632e4130bdf2c9a1a0bf9fc52
D3adIns1de <alex_tokarev01@mail.ru>
2022-09-08 00:16:53 +0400

    commit-photos
    add photos to the NewArrivals

--- pet-project/src/components/NewArrivals/NewArrivals.tsx
+++ pet-project/src/components/NewArrivals/NewArrivals.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import styles from "./NewArrivals.module.css";
+import NewArrivals__Photos from "./NewArrivals__Photos/NewArrivals__Photos";
+import NewArrivals__Slider from "./NewArrivals__Slider/NewArrivals__Slider";
 const NewArrivals = () => {
   return (
-    <section>
-      <div></div>
-      <div></div>
+    <section className={styles.pageWrapper}>
+      <NewArrivals__Photos></NewArrivals__Photos>
+      <NewArrivals__Slider></NewArrivals__Slider>
     </section>
   );
 };
--- pet-project/src/components/NewArrivals/NewArrivals__Photos/NewArrivals__Photos.tsx
+++ pet-project/src/components/NewArrivals/NewArrivals__Photos/NewArrivals__Photos.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
-
+import React from "react";
+import styles from "./NewArrivals__Photos.module.css";
+import boy from "../../../img/NewArrivalsPhotos/boy.png";
+import girl from "../../../img/NewArrivalsPhotos/girl.png";
+import baby from "../../../img/NewArrivalsPhotos/baby.png";
 const NewArrivals__Photos = () => {
-  return (
-    <div>NewArrivals__Photos</div>
-  )
-}
+  const imgContainer = [girl, boy, baby];
+  const names = ["Women's", "Men's", "Kids'"];
+  const photo = imgContainer.map((item) => (
+    <a className={styles.photoItem}>
+      <img src={item}></img>
+      <a className={styles.photoLabel}>{names[imgContainer.indexOf(item)]}</a>
+    </a>
+  ));
+  return <div className={styles.photoWrapper}>{photo}</div>;
+};
 
-export default NewArrivals__Photos
+export default NewArrivals__Photos;
