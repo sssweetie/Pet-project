c4069cc163bcd89fa8e694fb9107a58a2528ef1c
sweetie <63711180+sssweetie@users.noreply.github.com>
2022-10-11 23:46:54 +0300

    commit-header-selector
    add selector in main page header

--- src/components/MainPage/MainPage__Header/MainPage__Header.tsx
+++ src/components/MainPage/MainPage__Header/MainPage__Header.tsx
@@ -1,6 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./MainPage__Header.module.css";
+import ru from "../../../img/Header/ru.svg";
+import usa from "../../../img/Header/usa.svg";
 const MainPage__Header = () => {
+  const languageSelector = ["Eng / $", "Rus / ₽"];
+  const flagSelector = [usa, ru];
+  const selector = [];
+  const [value, setValue] = useState(0);
+  for (let i = 0; i < languageSelector.length; i++) {
+    selector.push(<option>{languageSelector[i]}</option>);
+  }
+
+  const changeFlag = (target: any) => {
+    setValue(target.selectedIndex);
+  };
+
   return (
     <header id="top" className={styles.header}>
       <div className={styles.container}>
@@ -14,9 +28,16 @@ const MainPage__Header = () => {
           <a>Blog</a>
           <a>Contacts</a>
         </nav>
-        <select>
-          <option>Eng / $</option>
-        </select>
+        <div className={styles.selectorWrapper}>
+          <img src={flagSelector[value]}></img>
+          <select
+            onChange={(e) => {
+              changeFlag(e.target);
+            }}
+          >
+            {selector}
+          </select>
+        </div>
         <a>Log In / Register</a>
       </div>
     </header>
